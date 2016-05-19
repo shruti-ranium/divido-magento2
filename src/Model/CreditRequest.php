a123a5bd86fcf844fb13b6e33754cc108aba5716
Jonas Hässel <i@cjkg.me>
2016-05-19 19:49:53 +0200

    Status fixes
    Exit with version
    Set status processing or custom status when contracts are signed,
    otherwise pending
    Remove status deferred
    Dont create order for DECLINED or CANCELED
    Cancel order on DECLINED after REFERRED

--- src/Model/CreditRequest.php
+++ src/Model/CreditRequest.php
@@ -7,56 +7,64 @@ use Divido\DividoFinancing\Api\CreditRequestInterface;
 class CreditRequest implements CreditRequestInterface
 {
     const
-        STATUS_ACCEPTED     = 'ACCEPTED',
-        STATUS_CANCELED     = 'CANCELED',
-        STATUS_COMPLETED    = 'COMPLETED',
-        STATUS_DEFERRED     = 'DEFERRED',
-        STATUS_DECLINED     = 'DECLINED',
-        STATUS_DEPOSIT_PAID = 'DEPOSIT-PAID',
-        STATUS_FULFILLED    = 'FULFILLED',
-        STATUS_REFERRED     = 'REFERRED',
-        STATUS_SIGNED       = 'SIGNED';
+        VERSION              = 'M2-1.0.0',
+        NEW_ORDER_STATUS     = 'processing',
+        STATUS_ACCEPTED      = 'ACCEPTED',
+        STATUS_ACTION_LENDER = 'ACTION-LENDER',
+        STATUS_CANCELED      = 'CANCELED',
+        STATUS_COMPLETED     = 'COMPLETED',
+        STATUS_DECLINED      = 'DECLINED',
+        STATUS_DEPOSIT_PAID  = 'DEPOSIT-PAID',
+        STATUS_FULFILLED     = 'FULFILLED',
+        STATUS_REFERRED      = 'REFERRED',
+        STATUS_SIGNED        = 'SIGNED';
 
+    private $historyMessages = [ 
+        self::STATUS_ACCEPTED      => 'Credit request accepted',
+        self::STATUS_ACTION_LENDER => 'Lender notified',
+        self::STATUS_CANCELED      => 'Application canceled',
+        self::STATUS_COMPLETED     => 'Application completed',
+        self::STATUS_DECLINED      => 'Applicaiton declined by Underwriter',
+        self::STATUS_DEPOSIT_PAID  => 'Deposit paid by customer',
+        self::STATUS_FULFILLED     => 'Credit request fulfilled',
+        self::STATUS_REFERRED      => 'Credit request referred by Underwriter, waiting for new status',
+        self::STATUS_SIGNED        => 'Customer have signed all contracts',
+    ];
 
-    private $history_messages = array(
-        self::STATUS_ACCEPTED     => 'Credit request accepted',
-        self::STATUS_CANCELED     => 'Application canceled',
-        self::STATUS_COMPLETED    => 'Application completed',
-        self::STATUS_DEFERRED     => 'Application deferred by Underwriter, waiting for new status',
-        self::STATUS_DECLINED     => 'Applicaiton declined by Underwriter',
-        self::STATUS_DEPOSIT_PAID => 'Deposit paid by customer',
-        self::STATUS_FULFILLED    => 'Credit request fulfilled',
-        self::STATUS_REFERRED     => 'Credit request referred by Underwriter, waiting for new status',
-        self::STATUS_SIGNED       => 'Customer have signed all contracts',
-    );
+    private $noGo = [
+        self::STATUS_CANCELED,
+        self::STATUS_DECLINED,
+    ];
 
     private 
+        $req,
+        $quote,
+        $order,
         $helper,
         $logger,
-        $req,
+        $config,
         $lookupFactory,
-        $quote,
-        $quoteManagement,
-        $order;
+        $quoteManagement;
 
     public function __construct(
-        \Magento\Framework\App\Request\Http $request,
-        \Magento\Sales\Model\Order $order,
-        \Magento\Quote\Model\Quote $quote,
-        \Magento\Quote\Model\QuoteManagement $quoteManagement,
-        \Divido\DividoFinancing\Helper\Data $helper,
-        \Divido\DividoFinancing\Model\LookupFactory $lookupFactory,
-        \Psr\Log\LoggerInterface            $logger
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\App\Request\Http                $request,
+        \Magento\Sales\Model\Order                         $order,
+        \Magento\Quote\Model\Quote                         $quote,
+        \Magento\Quote\Model\QuoteManagement               $quoteManagement,
+        \Divido\DividoFinancing\Helper\Data                $helper,
+        \Divido\DividoFinancing\Model\LookupFactory        $lookupFactory,
+        \Psr\Log\LoggerInterface                           $logger
     ) {
         $this->req    = $request;
-        $this->helper = $helper;
-        $this->logger = $logger;
-        $this->quoteManagement = $quoteManagement;
         $this->quote = $quote;
         $this->order = $order;
+        $this->helper = $helper;
+        $this->logger = $logger;
+        $this->config = $scopeConfig;
         $this->lookupFactory = $lookupFactory;
+        $this->quoteManagement = $quoteManagement;
     }
-
 
     /**
      * Create a credit request as Divido, return a URL to complete the credit 
@@ -101,20 +109,41 @@ class CreditRequest implements CreditRequestInterface
         $lookup = $this->lookupFactory->create()->load($quoteId, 'quote_id');
         if (! $lookup->getId()) {
             $this->logger->addError('Divido: Bad request, could not find lookup. Req: ' . $content);
-            exit('Cannot verify request');
+            exit('Cannot verify request ' . self::VERSION);
         }
 
         $salt = $lookup->getSalt();
         $hash = $this->helper->hashQuote($salt, $data->metadata->quote_id);
         if ($hash !== $data->metadata->quote_hash) {
             $this->logger->addError('Divido: Bad request, mismatch in hash. Req: ' . $content);
-            exit('Cannot verify request');
+            exit('Cannot verify request' . self::VERSION);
         }
 
         $lookup->setData('application_id', $data->application);
         $lookup->save();
 
         $order = $this->order->loadByAttribute('quote_id', $quoteId);
+
+        if (! $order->getId() && in_array($data->status, $this->noGo)) {
+            if ($data->status == self::STATUS_DECLINED) {
+                $lookup->setData('declined', 1);
+            } elseif ($data->status == self::STATUS_CANCELED) {
+                $lookup->setData('canceled', 1);
+            }
+            $lookup->save();
+
+            return self::VERSION;
+        }
+
+        if ($order->getId() && $data->status == self::STATUS_DECLINED) {
+            $order->addStatusHistoryComment($this->historyMessages[$data->status]);
+            $order->cancel();
+            $order->save();
+
+            $lookup->setData('declined', 1);
+            $lookup->save();
+            return self::VERSION;
+        }
 
         if (! $order->getId()) {
             $quote = $this->quote->loadActive($quoteId);
@@ -126,16 +155,32 @@ class CreditRequest implements CreditRequestInterface
             $orderId = $this->quoteManagement->placeOrder($quoteId);
 
             $order = $this->order->load($orderId);
+        } else {
+            $orderId = $order->getId();
         }
 
-        $comment = 'Divido: Unknown status';
-        if (array_key_exists($data->status, $this->history_messages)) {
-            $comment = 'Divido: ' . $this->history_messages[$data->status];
+        $lookup->setData('order_id', $orderId);
+        $lookup->save();
+
+        xdebug_break();
+        if ($data->status == self::STATUS_SIGNED) {
+            $status = self::NEW_ORDER_STATUS;
+            $status_override = $this->config->getValue('payment/divido_financing/order_status',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            if ($status_override) {
+                $status = $status_override;
+            }
+            $order->setStatus($status);
+        }
+
+        $comment = 'Divido: ' . $data->status;
+        if (array_key_exists($data->status, $this->historyMessages)) {
+            $comment = 'Divido: ' . $this->historyMessages[$data->status];
         }
 
         $order->addStatusHistoryComment($comment);
         $order->save();
 
-        return 'm2-1.0';
+        return self::VERSION;
     }
 }
