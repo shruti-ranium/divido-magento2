88769822e912e98d9612cfdf94cf1857968e83e7
Jonas Hässel <i@cjkg.me>
2016-12-06 15:56:14 +0100

    New source for Divido Statuses

---
+++ Model/Adminhtml/Source/DividoStatus.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © 2016 Divido. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Divido\DividoFinancing\Model\Adminhtml\Source;
+
+/**
+ * Class DividoStatus
+ */
+class DividoStatus implements \Magento\Framework\Option\ArrayInterface
+{
+    const
+        STATUS_ACCEPTED = 'ACCEPTED',
+        STATUS_SIGNED   = 'SIGNED';
+
+
+
+    public function toOptionArray ()
+    {
+        return [
+            [
+                'value' => self::STATUS_ACCEPTED,
+                'label' => 'Accepted',
+            ],
+            [
+                'value' => self::STATUS_SIGNED,
+                'label' => 'Signed',
+            ],
+        ];
+    }
+}
